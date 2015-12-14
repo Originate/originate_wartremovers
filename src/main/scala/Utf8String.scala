7c0763cbd194fde86498c470b06fdf608ddd8445
matt handler <matt.handler@gmail.com>
2015-12-14 14:24:06 -0500

    create Utf8String wart remover and spec

--- src/main/scala/Utf8String.scala
+++ src/main/scala/Utf8String.scala
@@ -1,96 +1,33 @@
-package ai.somatix.wartremovers
+package com.originate.wartremovers
 
 import org.brianmckenna.wartremover.{WartTraverser, WartUniverse}
 
 object Utf8String extends WartTraverser {
+
+  val StringGetBytesError = """Use explicit encoding flag, ex: "string".getBytes(UTF_8)"""
+  val StringConstructorWithBytesError =
+    """Use explicit encoding flag when constructing strings, ex: new String(bytes, UTF_8)"""
+
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
-    val StringClass = rootMirror.staticModule("scala.collection.immutable.StringOps")
-    val GetBytesMethodName = "getBytes"
+
+    val GetBytesMethodName = TermName("getBytes")
 
     new u.Traverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
-          // Ignore trees marked by SuppressWarnings
           case t if hasWartAnnotation(u)(t) =>
-          case Select(tpt, GetBytesMethodName) if tpt.tpe.contains(StringClass) =>
-            u.error(tree.pos, "this shit sucks")
+          case Apply(Select(left, GetBytesMethodName), args) if left.tpe <:< typeOf[String] && args.isEmpty =>
+            u.error(tree.pos, StringGetBytesError)
             super.traverse(tree)
-          case _ => super.traverse(tree)
+          case q"new scala.this.Predef.String(..$args)" if args.size <= 1 =>
+            u.error(tree.pos, StringConstructorWithBytesError)
+            super.traverse(tree)
+          case a =>
+            super.traverse(tree)
         }
       }
     }
   }
+
 }
-
-
-// object AsInstanceOf extends WartTraverser {
-//   def apply(u: WartUniverse): u.Traverser = {
-//     import u.universe._
-//     val EqualsName: TermName = "equals"
-//     val AsInstanceOfName: TermName = "asInstanceOf"
-
-//     val allowedCasts = List(
-//       "scala.tools.nsc.interpreter.IMain" // REPL needs this
-//     ) // cannot do `map rootMirror.staticClass` here because then:
-//       //   scala.ScalaReflectionException: object scala.tools.nsc.interpreter.IMain in compiler mirror not found.
-
-//     new u.Traverser {
-//       override def traverse(tree: Tree): Unit = {
-//         val synthetic = isSynthetic(u)(tree)
-//         tree match {
-
-//           // Ignore trees marked by SuppressWarnings
-//           case t if hasWartAnnotation(u)(t) =>
-
-//           // Ignore usage in synthetic classes
-//           case ClassDef(_, _, _, _) if synthetic =>
-
-//           // Ignore synthetic equals()
-//           case DefDef(_, EqualsName, _, _, _, _) if synthetic =>
-
-//           // Pattern matcher writes var x1 = null.asInstanceOf[...]
-//           case ValDef(mods, _, _, _) if mods.hasFlag(Flag.MUTABLE) && synthetic =>
-
-//           // Ignore allowed casts
-//           case TypeApply(Select(_, AsInstanceOfName), List(tt))
-//             if tt.isType && allowedCasts.contains(tt.tpe.typeSymbol.fullName) =>
-
-//           // Otherwise it's verboten for non-synthetic exprs
-//           case Select(e, AsInstanceOfName) if !isSynthetic(u)(e) =>
-//             u.error(tree.pos, "asInstanceOf is disabled")
-
-//           case _ => super.traverse(tree)
-
-//         }
-//       }
-//     }
-//   }
-// }
-
-
-
-// package org.brianmckenna.wartremover
-// package warts
-
-// object JavaConversions extends WartTraverser {
-//   def apply(u: WartUniverse): u.Traverser = {
-//     import u.universe._
-
-//     val javaConversions = rootMirror.staticModule("scala.collection.JavaConversions")
-
-//     new u.Traverser {
-//       override def traverse(tree: Tree): Unit = {
-//         tree match {
-//           // Ignore trees marked by SuppressWarnings
-//           case t if hasWartAnnotation(u)(t) =>
-//           case Select(tpt, _) if tpt.tpe.contains(javaConversions) => {
-//             u.error(tree.pos, "scala.collection.JavaConversions is disabled - use scala.collection.JavaConverters instead")
-//             super.traverse(tree)
-//           }
-//           case _ => super.traverse(tree)
-//         }
-//       }
-//     }
-//   }
-// }
---
+++ src/test/scala/Utf8StringSpec.scala
@@ -0,0 +1,74 @@
+package com.originate.wartremovers
+
+import org.brianmckenna.wartremover.test.WartTestTraverser
+import org.scalatest.FreeSpec
+
+import java.nio.charset.StandardCharsets.UTF_8
+
+class Utf8StringSpec extends FreeSpec {
+
+  "obeys SuppressWarnings" in {
+    val result = WartTestTraverser(Utf8String) {
+      @SuppressWarnings(Array("com.originate.wartremovers.Utf8String"))
+      def test = "some string".getBytes
+    }
+    assertResult(List.empty, "result.errors")(result.errors)
+    assertResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  "String#getBytes" - {
+
+    "on literals" - {
+
+      "does not compile" in {
+        val result = WartTestTraverser(Utf8String) {
+          "some string".getBytes
+        }
+        assertResult(List(Utf8String.StringGetBytesError), "result.errors")(result.errors)
+      }
+
+      "compiles with an explicit flag" in {
+        val result = WartTestTraverser(Utf8String) {
+          "some string".getBytes(UTF_8)
+        }
+        assertResult(List.empty, "result.errors")(result.errors)
+      }
+
+    }
+
+    "on string variables" - {
+
+      "does not compile" in {
+        val result = WartTestTraverser(Utf8String) {
+          val x = "some string"
+          x.getBytes
+        }
+        assertResult(List(Utf8String.StringGetBytesError), "result.errors")(result.errors)
+      }
+
+    }
+
+  }
+
+  "String constructor" - {
+
+    "compiles with an encoding flag" in {
+      val result = WartTestTraverser(Utf8String) {
+        val bytes = "ok".toCharArray map (_.toByte)
+        new String(bytes, UTF_8)
+      }
+      assertResult(List.empty, "result.errors")(result.errors)
+      assertResult(List.empty, "result.warnings")(result.warnings)
+    }
+
+    "does not compile without an encoding flag" in {
+      val result = WartTestTraverser(Utf8String) {
+        val bytes = "ok".toCharArray map (_.toByte)
+        new String(bytes)
+      }
+      assertResult(List(Utf8String.StringConstructorWithBytesError), "result.errors")(result.errors)
+    }
+
+  }
+
+}
